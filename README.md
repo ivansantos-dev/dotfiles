86d82ceaf0a8131442e0caf5a25822dd8269db6c
Ivan Santos <me@ivan-santos.dev>
2023-02-28 17:39:04 -0700

    Add .tmux.conf and remove iTerm

---
+++ README.md
@@ -0,0 +1,10 @@
+# My files in .config
+
+
+Have installed 
+* fish
+* asdf
+* brew
+
+Run 
+`bash install.sh`
---
+++ install.sh
@@ -0,0 +1,3 @@
+ln -s ~/Developer/dotfiles/fish ~/.config/fish
+ln -s ~/Developer/dotfiles/.tmux.config ~/.tmux.conf
+
